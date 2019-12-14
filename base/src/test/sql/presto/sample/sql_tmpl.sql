a0eafbdee2fc5b33bca7ed3438d88f9ee0896132
Taro L. Saito <leo@xerial.org>
2019-12-13 17:48:32 -0800

    Fix dummy query

--- base/src/test/sql/presto/sample/sql_tmpl.sql
+++ base/src/test/sql/presto/sample/sql_tmpl.sql
@@ -1,4 +1,4 @@
-@(start:String, end:String, cond:sql="AND time > 0")
+@(start:String="2019-01-01", end:String="2019-01-02", cond:sql="AND time > 0")
 select * from sample_datasets.nasdaq
 where TD_TIME_RANGE(time, '${start}', '${end}')
 ${cond}
